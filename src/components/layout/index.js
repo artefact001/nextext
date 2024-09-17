e7d7275bc7fc1aa0343c851ae7b9685387c0f0d2
souleymane97 <souleymane9700@gmail.com>
2024-09-17 10:31:27 +0000

    fix: config

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,10 +1,8 @@
-import Navbar from './Navbar';
 import Footer from './Footer';
 
 export const Layout = ({ active, children }) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar active={active} />
       <main className="flex-grow container mx-auto p-4">{children}</main>
       <Footer />
     </div>
